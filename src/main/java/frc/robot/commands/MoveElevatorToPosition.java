de9704a9219e12fff4c6b85896d4a9f2e2f1a525
josie-miller <josephinemiller13524@icloud.com>
2024-06-01 23:18:39 -0700

    elevatoring

--- src/main/java/frc/robot/commands/MoveElevatorToPosition.java
+++ src/main/java/frc/robot/commands/MoveElevatorToPosition.java
@@ -1,35 +1,21 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.elevator.Elevator;
 
-public class MoveElevatorToPosition extends Command {
-    private final Elevator elevator;
-    private final double targetPosition;
+public class MoveElevatorToPosition extends InstantCommand {
 
     public MoveElevatorToPosition(Elevator elevator, double targetPosition) {
-        this.elevator = elevator;
-        this.targetPosition = targetPosition;
-        addRequirements(elevator);
-    }
-
-    @Override
-    public void initialize() {
-        elevator.setSetpoint(targetPosition);
-        elevator.enable();
-    }
-
-    @Override
-    public void execute() {
+        super(() -> {
+            elevator.setSetpoint(targetPosition);
+            elevator.enable();
+        }, elevator);
     }
 
     @Override
     public void end(boolean interrupted) {
-        elevator.disable();
-    }
-
-    @Override
-    public boolean isFinished() {
-        return elevator.atSetpoint();
+        if (interrupted) {
+            elevator.disable();
+        }
     }
 }
