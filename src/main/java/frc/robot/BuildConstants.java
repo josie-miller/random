d6ce4f8cac024297110dfffc40f18820aaaa4b5f
FRC 9400 <josephinemiller13524@icloud.com>
2024-06-24 08:23:50 -0700

    uhh

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "random";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 24;
-  public static final String GIT_SHA = "9b66bc65ee414cd7ffcd08ce825b5c7530558927";
-  public static final String GIT_DATE = "2024-06-11 14:52:04 EDT";
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "3168639d97aa955201a8cc222e5b12439619db91";
+  public static final String GIT_DATE = "2024-06-14 18:53:24 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-06-11 15:47:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1718135233690L;
+  public static final String BUILD_DATE = "2024-06-15 18:41:02 EDT";
+  public static final long BUILD_UNIX_TIME = 1718491262135L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
