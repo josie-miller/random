d957ffc375fb5e6fc7c85878f25f0d0e31deb6a4
josie-miller <josephinemiller13524@icloud.com>
2024-07-12 08:53:37 -0400

    um

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "random";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "55f984b0dc7c7f6a4cda730a7e867f87ffc4ccb8";
-  public static final String GIT_DATE = "2024-07-11 11:04:42 EDT";
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "4f52da676e6e9a529ae831023fb2edc8ef21a726";
+  public static final String GIT_DATE = "2024-07-11 11:40:03 EDT";
   public static final String GIT_BRANCH = "new";
-  public static final String BUILD_DATE = "2024-07-11 11:37:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1720712257885L;
+  public static final String BUILD_DATE = "2024-07-12 08:52:33 EDT";
+  public static final long BUILD_UNIX_TIME = 1720788753548L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import frc.robot.subsystems.elevator.Elevator;
