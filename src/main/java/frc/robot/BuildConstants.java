de5a49756b9bcb712da2c4559e2e90c649ac1cdc
josie-miller <josephinemiller13524@icloud.com>
2024-07-21 20:17:21 -0700

    fuck

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "random";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 51;
-  public static final String GIT_SHA = "24dfe2385413d3113f5d786c53d990f097a9a15c";
-  public static final String GIT_DATE = "2024-07-15 10:52:02 EDT";
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "a85708528134b0715a51fdb6e21fae3de30a6a0b";
+  public static final String GIT_DATE = "2024-07-21 19:43:15 EDT";
   public static final String GIT_BRANCH = "choreo";
-  public static final String BUILD_DATE = "2024-07-17 10:09:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1721225386304L;
+  public static final String BUILD_DATE = "2024-07-21 20:31:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1721608289838L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,46 +1,91 @@
 package frc.robot.subsystems.elevator;
 
-import org.littletonrobotics.junction.Logger;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import frc.robot.Constants;
+import frc.robot.Conversions;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.commons.Conversions;
-import frc.robot.constants.elevatorConstants;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
 public class Elevator extends SubsystemBase {
-    private final ElevatorIO elevatorIO;
-    private ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+    private final TalonFX leftMotor;
+    private final TalonFX rightMotor;
+    private final StatusSignal<Double> current1;
+    private final StatusSignal<Double> temp1;
+    private final StatusSignal<Double> RPS1;
+    private final StatusSignal<Double> position1;
     private double setpointMeters;
 
-    public Elevator(ElevatorIO elevatorIO) {
-        this.elevatorIO = elevatorIO;
+    public Elevator() {
+        leftMotor = new TalonFX(Constants.canIDConstants.elevatorMotor1, "canivore");
+        rightMotor = new TalonFX(Constants.canIDConstants.elevatorMotor2, "canivore");
+
+        current1 = leftMotor.getStatorCurrent();
+        temp1 = leftMotor.getDeviceTemp();
+        RPS1 = leftMotor.getRotorVelocity();
+        position1 = leftMotor.getPosition();
+
+        var leftMotorConfigs = new TalonFXConfiguration();
+        leftMotorConfigs.CurrentLimits.StatorCurrentLimit = Constants.elevatorConstants.statorCurrentLimit;
+        leftMotorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+        leftMotorConfigs.MotorOutput.Inverted = Constants.elevatorConstants.leftMotorInvert;
+        leftMotorConfigs.MotionMagic.MotionMagicCruiseVelocity = Constants.elevatorConstants.CruiseVelocityUp;
+        leftMotorConfigs.MotionMagic.MotionMagicAcceleration = Constants.elevatorConstants.AccelerationUp;
+        leftMotorConfigs.Slot0.kP = Constants.elevatorConstants.kP;
+        leftMotorConfigs.Slot0.kD = Constants.elevatorConstants.kD;
+        leftMotorConfigs.Slot0.kS = Constants.elevatorConstants.kS;
+        leftMotorConfigs.Slot0.kV = Constants.elevatorConstants.kV;
+        leftMotorConfigs.Slot0.kG = Constants.elevatorConstants.kG;
+
+        leftMotor.getConfigurator().apply(leftMotorConfigs);
+
+        rightMotor.setControl(new Follower(leftMotor.getDeviceID(), true));
+
+        BaseStatusSignal.setUpdateFrequencyForAll(
+            50,
+            current1,
+            temp1,
+            RPS1,
+            position1
+        );
+
+        leftMotor.optimizeBusUtilization();
+        rightMotor.optimizeBusUtilization();
+
+        setpointMeters = 0;
     }
 
     public void setSetpoint(double setpointMeters) {
         this.setpointMeters = setpointMeters;
-        double setpointRotations = Conversions.metersToRotations(setpointMeters, elevatorConstants.wheelCircumferenceMeters, elevatorConstants.gearRatio);
-        elevatorIO.setMotionMagicSetpoint(setpointRotations);
+        double setpointRotations = Conversions.metersToRotations(setpointMeters, Constants.elevatorConstants.wheelCircumferenceMeters, Constants.elevatorConstants.gearRatio);
+        leftMotor.setControl(new MotionMagicVoltage(setpointRotations));
     }
 
-    public void Loop() {
-        elevatorIO.updateInputs(inputs);
-        Logger.processInputs("Elevator", inputs);
+    @Override
+    public void periodic() {
+        BaseStatusSignal.refreshAll(current1, temp1, RPS1, position1);
+        SmartDashboard.putNumber("Elevator Position", position1.getValue());
+        SmartDashboard.putNumber("Elevator Current", current1.getValue());
+        SmartDashboard.putNumber("Elevator Temperature", temp1.getValue());
+        SmartDashboard.putNumber("Elevator Speed (RPS)", RPS1.getValue());
     }
 
     public void zeroSensor() {
-        elevatorIO.zeroSensor();
+        leftMotor.setPosition(0);
     }
 
     public boolean atSetpoint() {
-        return Math.abs(inputs.elevatorHeightMeters - setpointMeters) < elevatorConstants.ToleranceMeters;
-    }
-
-    public double getPosition() {
-        return inputs.elevatorHeightMeters;
+        double currentPosMeters = Conversions.rotationsToMeters(position1.getValue(), Constants.elevatorConstants.wheelCircumferenceMeters, Constants.elevatorConstants.gearRatio);
+        return Math.abs(currentPosMeters - setpointMeters) < Constants.elevatorConstants.ToleranceMeters; 
     }
 
     public void disable() {
-        elevatorIO.runElevator(0);
-    }
-    public void updateInputs(ElevatorIO.ElevatorIOInputs inputs) {
-        elevatorIO.updateInputs(inputs);
+        leftMotor.setVoltage(0);
+        rightMotor.setVoltage(0);
     }
 }
