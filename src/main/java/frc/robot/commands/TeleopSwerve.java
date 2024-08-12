b2b2c7d5c487b5aee3d6b30697ae2ddb36769410
josie-miller <144562888+josie-miller@users.noreply.github.com>
2024-08-11 21:08:52 -0700

    swerve

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -1,43 +1,38 @@
 package frc.robot.commands;
 
 import frc.robot.Constants.swerveConstants;
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
-
 import frc.robot.subsystems.swerve.Swerve;
 
-
+import java.util.function.DoubleSupplier;
 
 public class TeleopSwerve extends Command {    
-    private Swerve swerve;    
-    private DoubleSupplier translationSup;
-    private DoubleSupplier strafeSup;
-    private DoubleSupplier rotationSup;
+    private final Swerve swerveSubsystem;    
+    private final DoubleSupplier translationSup;
+    private final DoubleSupplier strafeSup;
+    private final DoubleSupplier rotationSup;
 
-
-    public TeleopSwerve(Swerve swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup) {
-        this.swerve = swerve;
+    public TeleopSwerve(Swerve swerveSubsystem, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup) {
+        this.swerveSubsystem = swerveSubsystem;
         this.translationSup = translationSup;
         this.strafeSup = strafeSup;
         this.rotationSup = rotationSup;
 
-        addRequirements(swerve);
+        addRequirements(swerveSubsystem);
     }
 
     @Override
     public void execute() {
         double translationVal = Math.pow(MathUtil.applyDeadband(translationSup.getAsDouble(), 0.05), 3); 
-        double strafeVal = Math.pow(MathUtil.applyDeadband(strafeSup.getAsDouble(), 0.05), 3) ; 
-        double rotationVal = Math.pow(MathUtil.applyDeadband(rotationSup.getAsDouble(), 0.05), 1); 
+        double strafeVal = Math.pow(MathUtil.applyDeadband(strafeSup.getAsDouble(), 0.05), 3); 
+        double rotationVal = Math.pow(MathUtil.applyDeadband(rotationSup.getAsDouble(), 0.05), 1);
 
         double x_speed = translationVal * swerveConstants.maxSpeed;
         double y_speed = strafeVal * swerveConstants.maxSpeed;
         double rot_speed = rotationVal * swerveConstants.maxAngularVelocity;
 
-        /* Drive */
-        swerve.requestDesiredState(
+        swerveSubsystem.requestDesiredState(
             x_speed, 
             y_speed,
             rot_speed, 
