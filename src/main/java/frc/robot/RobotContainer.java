0521596e49f50333f5fe2688712afa7d3b7ade08
josie-miller <josephinemiller13524@icloud.com>
2024-08-20 15:45:30 -0700

    tuning

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,15 +19,6 @@ import frc.robot.subsystems.otb_intake.OtbIntakeIOReal;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.ShooterIOReal;
 import frc.robot.subsystems.swerve.Swerve;
-import frc.robot.autons.AutonomousSelector;
-import frc.robot.autons.AutonomousSelector.modes;
-import frc.robot.commands.TeleopSwerve;
-import frc.robot.commands.sequences.IntakeSequence;
-import frc.robot.commands.sequences.ShootSequence;
-import frc.robot.commands.singles.RunHandoff;
-import frc.robot.commands.singles.RunIntake;
-import frc.robot.commands.singles.RunIntake2;
-import frc.robot.commands.singles.SetPivot;
 import frc.robot.constants.commandConstants;
 
 public class RobotContainer {
@@ -37,81 +28,40 @@ public class RobotContainer {
     private final Shooter shooter = new Shooter(new ShooterIOReal());
     private final OtbIntake otbIntake = new OtbIntake(new OtbIntakeIOReal());
     private final Swerve swerve = new Swerve();
-    //public final CommandXboxController controller = new CommandXboxController(1);
     public final CommandXboxController operator = new CommandXboxController(0);
-    private AutonomousSelector selector;
 
     ChoreoTrajectory traj;
 
     public RobotContainer() {
     swerve.zeroWheels();
     swerve.zeroGyro();
-    swerve.setDefaultCommand(
-            new TeleopSwerve(
-                swerve, 
-                () -> operator.getRawAxis(XboxController.Axis.kLeftY.value),
-                () -> operator.getRawAxis(XboxController.Axis.kLeftX.value), 
-                () -> operator.getRawAxis(XboxController.Axis.kRightX.value)
-              
-            )
-        );
     
     configureButtonBindings();
     }
 
     private void configureButtonBindings() {
 
-        operator.a() //IDLE
-            .onTrue(new ParallelCommandGroup(
-                new SetPivot(otbIntake, commandConstants.restingDegrees),
-                new RunCommand(() -> handoff.runHandoff(0)),
-                new RunCommand(() -> intake.runIntake(0)),
-                new InstantCommand(() -> elevator.setSetpoint(0)),
-                new RunCommand(() -> shooter.setVelocity(0,0))
-            ) {{addRequirements(intake, handoff, otbIntake, elevator, shooter);}}
-        );
+        operator.a() 
+            .onTrue(elevator.runSysIdCmd());
 
-        operator.rightBumper() //INTAKE
-            .onTrue(new IntakeSequence(otbIntake, intake, handoff));
+        operator.b()
+            .onTrue(otbIntake.runSysIdCmd());
 
-        operator.x() //SHOOT SUBWOOFER AMP
-            .onTrue(new ShootSequence(intake, handoff, shooter, commandConstants.leftShootTime, commandConstants.leftShootRatio, commandConstants.handoffIntakeVoltage, commandConstants.handoffShooterVoltage, commandConstants.shootLeftVelocity, commandConstants.leftShortTime));
+        operator.x()
+            .onTrue(shooter.shooterSysIdCmd());
 
-        operator.y() //SHOOT SUBWOOFER MID
-            .onTrue(new ShootSequence(intake, handoff, shooter, commandConstants.midShootTime, commandConstants.midShootRatio, commandConstants.handoffIntakeVoltage, commandConstants.handoffShooterVoltage, commandConstants.shootMidVelocity, commandConstants.midShortTime));
-       
-        operator.b() //SHOOT SUBWOOFER SOURCE
-            .onTrue(new ShootSequence(intake, handoff, shooter, commandConstants.rightShootTime, commandConstants.rightShootRatio, commandConstants.handoffIntakeVoltage, commandConstants.handoffShooterVoltage, commandConstants.shootRightVelocity, commandConstants.rightShortTime));
-            
-        operator.leftBumper() //SHOOT AMP
-            .onTrue(new ShootSequence(intake, handoff, shooter, commandConstants.AMPShootTime, commandConstants.AMPShootRatio, commandConstants.handoffIntakeVoltage, commandConstants.handoffShooterVoltage, commandConstants.AMPShooterVelocity, commandConstants.AMPShortTime));
+        operator.rightTrigger()
+            .onTrue(swerve.driveSysIdCmd());
 
-        operator.leftTrigger() //OUTTAKE
-            .onTrue(new ParallelCommandGroup(
-                new SetPivot(otbIntake, commandConstants.restingDegrees),
-                new RunIntake2(intake, commandConstants.outakeVoltage, 2.0),
-                new RunHandoff(handoff, -2, 2.0)
-            ) {{addRequirements(intake, handoff, otbIntake);}}
-        );
-
-        operator.rightTrigger() //INTAKE W/O OTB
-            .onTrue(new ParallelCommandGroup(
-                new SetPivot(otbIntake, commandConstants.restingDegrees),
-                new RunIntake(intake, commandConstants.intakeVoltage)
-            ) {{addRequirements(intake, otbIntake);}});
-
-        
-        /*controller.b()
-            .onTrue(new InstantCommand(() -> swerve.zeroGyro()));*/
+        operator.leftTrigger()
+            .onTrue(swerve.steerSysIdCmd());
 
     }
 
-    public modes getAutonomousCommand() {
-        return selector.get();
+    public void getAutonomousCommand() {
     }
 
     public void configureAutonomousSelector(){
-    selector = new AutonomousSelector(swerve, intake, otbIntake, handoff, shooter);
     }
 
     public Swerve getSwerve(){
